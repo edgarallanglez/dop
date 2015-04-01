6dc24c32271eb0ba90bbcadd7ee68c7270e30982
jequintero <eduardo.quintero52@gmail.com>
2015-04-01 14:08:13 -0700

    Clean

--- app/scripts/controllers/dashboardController/couponStatusWidgetController.js
+++ app/scripts/controllers/dashboardController/couponStatusWidgetController.js
@@ -8,12 +8,24 @@
  * Controller of the dopApp
  */
 angular.module('dopApp')
-  .controller('CouponWidgetCtrl', ['$scope', function(scope) {
-    scope.awesomeThings = [
+  .controller('CouponWidgetCtrl', ['$scope', function($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.data = {
+      selectedIndex : 1,
+      secondLocked : true,
+      secondLabel : "Item Two"
+    };
+    $scope.next = function() {
+      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+    };
+    $scope.previous = function() {
+      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+    };
    
   
   }])
