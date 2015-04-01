1a4fef60c2afe389c9e99fcc559d0a9c6d391751
jequintero <eduardo.quintero52@gmail.com>
2015-04-01 13:08:37 -0700

    Coupon Widget View/Controller

---
+++ app/scripts/controllers/dashboardController/couponStatusWidgetController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dopApp.controller:CouponWidgetCtrl
+ * @description
+ * # CouponWidgetCtrl
+ * Controller of the dopApp
+ */
+angular.module('dopApp')
+  .controller('CouponWidgetCtrl', ['$scope', function(scope) {
+    scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+   
+  
+  }])
