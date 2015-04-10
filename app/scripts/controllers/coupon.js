ef2464ea2369b507df02536d9b54acb945605dba
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-10 17:35:33 -0600

    coupon refixed

--- app/scripts/controllers/coupon.js
+++ app/scripts/controllers/coupon.js
@@ -8,22 +8,22 @@
  */
 angular.module('dopApp')
 	.config(function($stateProvider){
-	    $stateProvider
-	        // HOME STATES AND NESTED VIEWS ========================================
-	        .state('coupon.list', {
-	          views: {
-	              // the child views will be defined here (absolutely named)
-	              'coupon-list-view': {
-	                templateUrl: '../../views/couponViews/couponListView.html',
-	                controller: 'CouponListCtrl'
-	              },
-                'coupon-main-view': {
-                  templateUrl: '../../views/couponViews/couponMainView.html',
-                  controller: 'CouponMainCtrl'
-                }
-	          }
-	      });
-	    })
+    $stateProvider
+      // HOME STATES AND NESTED VIEWS ========================================
+      .state('coupon.list', {
+        views: {
+          // the child views will be defined here (absolutely named)
+          'coupon-list-view': {
+            templateUrl: '../../views/couponViews/couponListView.html',
+            controller: 'CouponListCtrl'
+          },
+          'coupon-main-view': {
+            templateUrl: '../../views/couponViews/couponMainView.html',
+            controller: 'CouponMainCtrl'
+          }
+        }
+      });
+    })
   .controller('CouponCtrl', function ($scope,$state) {
   	$state.transitionTo('coupon.list');
     
