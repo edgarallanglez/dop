dcd9c92233eef4c346981c3206807d5b7faa29ee
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-27 13:19:51 -0600

    new cards dashboard

--- app/scripts/controllers/dashboardController/ageWidgetController.js
+++ app/scripts/controllers/dashboardController/ageWidgetController.js
@@ -14,7 +14,16 @@ angular.module('dopApp')
 
     $scope.labels = ['12-15', '16-19', '20-24', '25-29', '30-34', '35-39', '+40'];
     $scope.data = [[10, 43, 55, 47, 30, 24, 11]];
-    $scope.colours = ['#3F51B5'];
+    $scope.colours = [{
+      fillColor: '#03A9F4',
+      strokeColor: '#1976D2',
+      highlightFill: '#4FC3F7',
+      highlightStroke: '#03A9F4'
+    }];
+    // $scope.options = {
+    //   scaleGridLineColor : "white",
+    //   scaleFontColor: "white"
+    // }
     $scope.$broadcast("$reload", {});
 
   });
