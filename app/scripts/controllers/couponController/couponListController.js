e00c446feda37b00e443255760ff4661481caefd
Edgar Allan Glez <edgarallan182@gmail.com>
2015-04-27 17:27:00 -0600

    css coupon-dashboard

--- app/scripts/controllers/couponController/couponListController.js
+++ app/scripts/controllers/couponController/couponListController.js
@@ -12,45 +12,14 @@ angular.module('dopApp')
   })
   .controller('CouponListCtrl', function($scope,$http) {
     $scope.todos = [];
-    /*$scope.todos = [
-      {
-        'limit': 'Sin límite de personas',
-        'name': '20% Frapuccino',
-        'when': '3:08PM',
-        'end_date': "Aplicable hasta el 25 de mayo"
-      },
-      {
-        'limit': 'Sin límite de personas',
-        'name': '20% Frapuccino',
-        'when': '3:08PM',
-        'end_date': "Aplicable hasta el 25 de mayo"
-      },
-      {
-        'limit': 'Sin límite de personas',
-        'name': '20% Frapuccino',
-        'when': '3:08PM',
-        'end_date': "Aplicable hasta el 25 de mayo"
-      },
-      {
-        'limit': 'Sin límite de personas',
-        'name': '20% Frapuccino',
-        'when': '3:08PM',
-        'end_date': "Aplicable hasta el 25 de mayo"
-      },
-      {
-        'limit': 'Sin límite de personas',
-        'name': '20% Frapuccino',
-        'when': '3:08PM',
-        'end_date': "Aplicable hasta el 25 de mayo"
-      }
-    ];*/
-    $http.get('http://104.236.141.44:5000/api/coupon/get/all').
-      success(function(data, status, headers, config) {
+
+    $http.get('http://104.236.141.44:5000/api/coupon/get/all')
+    .success(function(data, status, headers, config) {
         angular.forEach(data.data, function(item) {
           $scope.todos.push(item);
         });     
-    }).
-    error(function(data, status, headers, config) {
+    })
+    .error(function(data, status, headers, config) {
       
     });
   })
