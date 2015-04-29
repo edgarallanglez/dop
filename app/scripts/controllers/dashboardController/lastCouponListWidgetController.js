21effb549faa267ea127c7823b31b3231024325c
jequintero <eduardo.quintero52@gmail.com>
2015-04-29 12:29:32 -0600

    clean

--- app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
+++ app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
@@ -11,16 +11,16 @@ angular.module('dopApp')
   .config(function($stateProvider){
   })
   .controller('LastCouponListWidgetCtrl', function($scope,$http) {
+
+    $scope.selectCoupon = function($event) {
+      console.log($event);
+    };
+
   	$scope.coupons = [];
 
    	$http.get('http://104.236.141.44:5000/api/coupon/get/all').
       success(function(data, status, headers, config) {
         angular.forEach(data.data, function(item) {
-        	if(item.limit==0)
-        		item.limit="Ilimitado";
-        	else
-        		item.limit="Para "+item.limit+" personas";
-        	
         	$scope.coupons.push(item);
         });     
     }).
