1b39fe0aa5addc4aae6650be3fdcfc21e02fea00
jequintero <eduardo.quintero52@gmail.com>
2015-04-29 17:17:00 -0600

    coupon factory

--- app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
+++ app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
@@ -10,17 +10,34 @@
 angular.module('dopApp')
   .config(function($stateProvider){
   })
+  .factory('$lastCouponFactory', function() {
+    this.isInView = false;
+    var coupon={
+      'name': 'nombre'
+    };
+   
+
+    this.setInView = function(currentStatus) {
+      this.isInView = currentStatus;
+    }
+
+    this.getCoupon = function(){
+      return this.isInView;
+    }
+
+    return coupon;
+  })
   .controller('LastCouponListWidgetCtrl', function($scope,$http,Restangular) {
 
-    $scope.selectCoupon = function($event) {
-      console.log($event);
-    };
+    $scope.selectCoupon = function(coupon) {
+      console.log(coupon);
+    }
 
   	$scope.coupons = [];
     Restangular.all('coupon/all/get').getList()
-    .then(function(data){
-      $scope.coupons = data;
-    });
+      .then(function(data){
+        $scope.coupons = data;
+      });
    /*	$http.get('http://104.236.141.44:5000/api/coupon/get/all').
       success(function(data, status, headers, config) {
         angular.forEach(data.data, function(item) {
