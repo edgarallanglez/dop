23d0aeaec2c6413b1145214994fcfc848d838851
edgarallang <edgarallan182@gmail.com>
2015-05-28 15:08:19 -0600

    coupons date

--- app/scripts/controllers/couponController/couponListController.js
+++ app/scripts/controllers/couponController/couponListController.js
@@ -16,21 +16,26 @@ angular.module('dopApp')
       this.coupon = coupon;
     }
   })
-  .config(function($stateProvider){
-  })
   .controller('CouponListCtrl', function($scope, $http, Restangular, SweetAlert, CouponFactory, $userService, $couponService) {
     var godCoupon = new CouponFactory();
     var branch_id = $userService.getCurrentUser().branch_id;
     
-
+    $scope.promod = new Date();
     $http({
       method: 'GET',
       url: 'http://104.236.141.44:5000/api/coupon/all/'+ branch_id + '/get',
     }).then(function(data){
       $scope.coupons = data.data;
+      console.log(data.data);
     });
 
-    
+    $scope.dateFormat = function(stringDate) {
+      if (stringDate) {
+        $scope.formattedDate = new Date(stringDate);
+        console.log($scope.formattedDate);
+      }
+    }
+
     $scope.delete = function(coupon) {
       godCoupon.deleteById(coupon.coupon_id);
     }
