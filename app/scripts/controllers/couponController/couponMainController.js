5cda82c2b23de8e27828d8292db8c80011a576fd
edgarallang <edgarallan182@gmail.com>
2015-08-13 12:31:48 -0600

    customizing promo

--- app/scripts/controllers/couponController/couponMainController.js
+++ app/scripts/controllers/couponController/couponMainController.js
@@ -13,7 +13,6 @@ angular.module('dopApp')
   .controller('CouponMainCtrl', function($scope, $http, $filter, SweetAlert, $userService, $couponService) {
     //var selected_coupon = Coupon.getCoupon();
    // console.log("Cupón seleccionado: "+selected_coupon.name);
-    $scope.couponSelected = 1;
     $scope.selectTags = [{
         'label': 'Compra X y llevate X',
         'val': 4 
@@ -27,14 +26,14 @@ angular.module('dopApp')
         'val': 2
     }];
   	$scope.coupon = { 
-      'startDate': new Date(), 
-		  'endDate': new Date(), 
+    //   'startDate': new Date(), 
+    // 'endDate': new Date(), 
       'name':'', 
-      'limit':0,
+      // 'limit':0,
       'min_spent':0,
       'receives':0,
       'buy':0,
-      'discount':0,
+      'percent':0,
       'bond':0,
       'description':'',
       'bond_size': 0
@@ -44,36 +43,28 @@ angular.module('dopApp')
       var couponInfo = {
         "name": $scope.coupon.name,
         "branch_id": $userService.getCurrentUser().branch_id,
-        "start_date": $filter('date')($scope.coupon.startDate,'MM-dd-yyyy'),
-        "end_date": $filter('date')($scope.coupon.endDate,'MM-dd-yyyy'),
         "min_spent": $scope.coupon.min_spent,
-        "limit": $scope.coupon.limit,
         "description": $scope.coupon.description,
         "coupon_category_id": $scope.couponSelected,
         "coupon_id": $couponService.coupon.coupon_id
       };
 
-      var createCouponUrl = 'http://45.55.7.118:5000/api/coupon/';
-
       switch($scope.couponSelected) {
         case 2:
           couponInfo.bond_size = $scope.coupon.bond_size;
-          createCouponUrl += "bond/create";
           break;
         case 3:
-          couponInfo.discount = $scope.coupon.discount;
-          createCouponUrl += "discount/create";
+          couponInfo.percent = $scope.coupon.percent;
           break;
         case 4:
           couponInfo.n1 = $scope.coupon.receives;
           couponInfo.n2 = $scope.coupon.buy;
-          createCouponUrl += "nxn/create";
           break;
       }
       
       $http({
         method: 'POST',
-        url: createCouponUrl,
+        url: 'http://45.55.7.118:5000/api/coupon/customize',
         data: couponInfo,
         headers: {'Content-Type': 'application/json'}
       })
@@ -81,7 +72,7 @@ angular.module('dopApp')
         SweetAlert.swal("Oops!", "Ha ocurrido un error, intentelo más tarde ", "error");
       })
       .finally(function() {
-        SweetAlert.swal("Cupón Creado!", "El cupón ha sido creado correctamente ", "success");
+        SweetAlert.swal("Cupón Creado!", "El cupón ha sido modificado correctamente ", "success");
       })
     };
 
@@ -91,6 +82,7 @@ angular.module('dopApp')
         if (flag) {
           $scope.coupon = $couponService.coupon;
           $scope.inSet = $couponService.inSet;
+          $scope.couponSelected = $scope.coupon.coupon_category_id
         }
     });
 
