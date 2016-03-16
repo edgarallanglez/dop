4c480d4efd63e0e68c458058774711e121d0fb80
jequintero <eduardo.quintero52@gmail.com>
2016-03-16 09:34:59 -0700

    cards

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -173,7 +173,8 @@ angular
 
     $urlRouterProvider.otherwise('/');
     // Theme configurations
-    $mdThemingProvider.theme('default')
+    
+    /*$mdThemingProvider.theme('default')
       .primaryPalette('blue-grey', {
         'default': '800',
         'hue-1': '100',
@@ -186,7 +187,35 @@ angular
       .primaryPalette('light-green')
       .accentPalette('light-blue');
 
-  })
+  })*/
+  $mdThemingProvider.definePalette('dopPalette', {
+    '50': 'ffebee',
+    '100': 'ffcdd2',
+    '200': 'ef9a9a',
+    '300': 'e57373',
+    '400': 'ef5350',
+    '500': 'ffffff',
+    '600': 'e53935',
+    '700': 'd32f2f',
+    '800': 'c62828',
+    '900': 'b71c1c',
+    'A100': 'fb226f',
+    'A200': 'ff5252',
+    'A400': 'ff1744',
+    'A700': 'd50000',
+    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+     '200', '300', '400', 'A100'],
+    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+  });
+  $mdThemingProvider.theme('default')
+    .primaryPalette('dopPalette', {
+        'default': '500',
+        'hue-1': '100',
+        'hue-2': '500'
+    })
+})
   .controller('MeCtrl', function($scope, $http, $auth, $userService, $mdSidenav, $log, $location){
     $scope.init = function () {
       if (!$userService.getCurrentUser() && $auth.isAuthenticated()) {
@@ -260,7 +289,8 @@ angular
       // tab selected change
       if ($scope.data) {
         if ($scope.data.selectedIndex == 0 && $auth.isAuthenticated() ) {
-          $location.path('/').replace();
+          $location.url("/");
+          //$location.path('/').replace();
         } else if ($scope.data.selectedIndex == 1) {
           $location.path('/coupon').replace();
         } else if ($scope.data.selectedIndex == 2) {
--- app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
+++ app/scripts/controllers/dashboardController/lastCouponListWidgetController.js
@@ -30,7 +30,7 @@ angular.module('dopApp')
     var branch_id = $userService.getCurrentUser().branch_id;
     $http({
       method: 'GET',
-      url: 'http://45.55.7.118:5000/api/coupon/all/'+ branch_id + '/get',
+      url: 'http://45.55.7.118:5000/api/coupon/latest/stats/'+ branch_id,
     }).then(function(data){
       $scope.coupons = data.data;
       $scope.loading = false;
