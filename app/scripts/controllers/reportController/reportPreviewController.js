4c7bc32a2983f83c196041bcd4370ed26b26a90d
jequintero <eduardo.quintero52@gmail.com>
2016-04-22 12:34:20 -0600

    clean

--- app/scripts/controllers/reportController/reportPreviewController.js
+++ app/scripts/controllers/reportController/reportPreviewController.js
@@ -10,20 +10,64 @@
 angular.module('dopApp')
   .config(function($stateProvider){
   })
-  .controller('ReportPreviewCtrl', function($scope, $http, $userService) {
+  .controller('ReportPreviewCtrl', function($scope, $http, $userService, $mdDialog, locals) {
     $scope.loading = true;
+    $scope.coupon = locals.coupon;
+
 
     var branch_id = $userService.getCurrentUser().branch_id;
 
     $http({
       method: 'GET',
-      url: 'http://45.55.7.118:5000/api/coupon/all/'+ branch_id + '/get',
+      url: 'http://45.55.7.118:5000/api/report/uses/'+ locals.coupon.coupon_id ,
     }).success(function(data) {
-      angular.forEach(data.data, function(value, key){
-        console.log(value.name);
-        $scope.loading = false;
+      $scope.loading = false;
+      $scope.coupons = data;
 
-        $scope.coupons = data;
-      });
+
+
     });
+    $scope.getPDF = function() {
+      var pdf = jsPDF('p', 'pt');
+      var myTables = pdf.autoTableHtmlToJson(document.getElementById('preview_table'));
+      delete myTables.data[0]
+      pdf.autoTable(myTables.columns, myTables.data,
+        {startY: 70, theme:'striped',
+          beforePageContent: function(data) {
+            pdf.setTextColor(40);
+            pdf.setFontStyle('normal');
+
+
+            var name = $scope.coupon.name;
+            var xOffset = (pdf.internal.pageSize.width / 2) - (pdf.getStringUnitWidth(name) * pdf.internal.getFontSize() / 2);
+            var available = 'Cupones disponibles: '+ $scope.coupon.available;
+            var remaining = 'Dias restantes: '+ $scope.coupon.available;
+
+            var diffDays = $scope.coupon.end_date - $scope.coupon.start_date;
+            alert(diffDays)
+
+            console.log($scope.coupon);
+
+            pdf.setFontSize(12);
+            pdf.text(name, xOffset, 28);
+            pdf.text(available, 40, 45);
+            pdf.text(remaining, 40, 60);
+          }
+      });
+
+
+      pdf.save($scope.coupon.name);
+
+      $mdDialog.hide();
+    };
+
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
   });
